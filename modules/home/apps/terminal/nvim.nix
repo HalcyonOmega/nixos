dab6b0e33591fd30983638ea0411e3ca7e0ea79c
HalcyonOmega <nathanbrown@me.com>
2026-05-15 06:10:42 -0400

    Adding customization

---
+++ modules/home/apps/terminal/nvim.nix
@@ -0,0 +1,11 @@
+{ username, ... }: {
+
+    home-manager.users.${username} = {
+        programs.neovim = {
+            enable = true;
+	    extraConfig = ''
+	      set number relativenumber
+	    '';
+        };
+    };
+}
