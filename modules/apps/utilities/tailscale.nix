a4ce50ee5a8206a4182aa7fbbe73555a47aafbf9
HalcyonOmega <nathanbrown@me.com>
2026-05-26 14:57:52 -0400

    tailscale

--- modules/apps/utilities/tailscale.nix
+++ modules/apps/utilities/tailscale.nix
@@ -8,8 +8,34 @@
           pkgs.tailscale
           pkgs.cifs-utils
         ];
-
-        services.tailscale.enable = true;
       };
+
+      services.tailscale.enable = true;
+
+      fileSystems."/home/${username}/Storehouse" = {
+        device = "//100.111.245.62/Storehouse";
+        fsType = "cifs";
+        options = [
+          "credentials=/etc/samba/credentials/storehouse"
+          "uid=${username}"
+          "gid=users"
+          "file_mode=0664"
+          "dir_mode=0775"
+          "iocharset=utf8"
+          "noperm"
+          "vers=3.0"
+          "x-systemd.automount"
+          "x-systemd.idle-timeout=60"
+          "noauto"
+          "nofail"
+          "_netdev"
+          "x-systemd.requires=tailscaled.service"
+          "x-systemd.after=tailscaled.service"
+        ];
+      };
+
+      systemd.tmpfiles.rules = [
+        "d /etc/samba/credentials 0700 root root -"
+      ];
     };
 }
